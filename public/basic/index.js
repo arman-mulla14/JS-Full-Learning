f15b12d35ce2f88d0aae045ca27079b5311910c2
Arman Mulla <armanbashirmulla.15@gmail.com>
2026-01-19 02:48:48 +0530

    data added

---
+++ public/basic/index.js
@@ -0,0 +1,23 @@
+// 
+let x = 10; 
+const y = 20; 
+var z = 30;
+console.log(x, y, z);
+
+console.log('This is a basic index.js file.');
+console.log(`your age is ${x}`)
+console.log(`your age is ${y}`)
+
+
+let a = 100;
+console.log(`your age is ${a}`)
+
+
+
+
+function roll(){
+alert("Welcome to the basic index.js file.");
+
+let head = document.getElementById('data'); 
+head.textContent = "Hello World!";
+}
